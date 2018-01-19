92e56f82b5b4816961b8791c9925dae9807c6d42
Sourav Kanta <sourav.kanta@gmail.com>
2018-01-19 15:45:03 +0530

    Mongo

--- app.js
+++ app.js
@@ -19,13 +19,13 @@ var pending = require('./routes/pending_actions');
 var app = express();
 var connection_string = '127.0.0.1:27017/jarvis-api';
 // if OPENSHIFT env variables are present, use the available connection info:
-if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+//if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
   connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
   process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
   process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
   process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
   process.env.OPENSHIFT_APP_NAME;
-}
+//}
 
 console.log(connection_string);
 mongoose.Promise = global.Promise;
